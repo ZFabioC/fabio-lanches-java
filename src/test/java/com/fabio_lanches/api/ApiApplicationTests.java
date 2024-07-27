a841be995ff957f28b3eb84a0cdc0232882eb709
Fabio <fabio_zanella00@gmail.com>
2024-07-27 09:51:28 -0300

    First Commit

---
+++ src/test/java/com/fabio_lanches/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio_lanches.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
